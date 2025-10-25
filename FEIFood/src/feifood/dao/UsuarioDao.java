2585fa72d2da8087799a28e16c9502d9eb0cd21a
Yuri <>
2025-10-25 20:59:28 -0300

    Métodos do UsuarioDao feito.

--- FEIFood/src/feifood/dao/UsuarioDao.java
+++ FEIFood/src/feifood/dao/UsuarioDao.java
@@ -1,5 +1,7 @@
 package feifood.dao;
 
+import feifood.model.Usuario;
+
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
@@ -9,11 +11,40 @@ import java.sql.Connection;
  * Classe responsável por abstrair o acesso aos usuários no banco de dados.
  */
 public class UsuarioDao
-{
-    private Conexao conexao;
+{   
+    private UsuarioDao() {}
     
-    public UsuarioDao(Conexao conexao)
+    public static ResultSet consultar(Usuario usuario) throws SQLException
     {
-        this.conexao = conexao;
+        Connection conexao = Conexao.getConexao();
+        
+        String sql = "select * from usuarios where nome = ? and senha = ?;";
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        
+        statement.setString(1, usuario.getNome());
+        statement.setString(2, usuario.getSenha());
+        
+        statement.execute();
+        
+        ResultSet resultado = statement.getResultSet();
+                
+        conexao.close();
+        
+        return resultado;
+    }
+    
+    public static void inserir(Usuario usuario) throws SQLException
+    {
+        Connection conexao = Conexao.getConexao();
+        
+        String sql = "insert into usuarios(nome, senha) values (?, ?);";
+        PreparedStatement statement = conexao.prepareStatement(sql);
+        
+        statement.setString(1, usuario.getNome());
+        statement.setString(2, usuario.getSenha());
+        
+        statement.execute();
+                
+        conexao.close();
     }
 }

e73965a8963df4eb2ecfa4480e905c3cb8fa5ee9
unifyshimoki <unifyshimoki@P2LB1007.FEINET.FEI.EDU.BR>
2025-11-13 14:34:45 -0300

    Adicionado javadoc e jar

--- FEIFood/src/feifood/view/CadastroFrame.java
+++ FEIFood/src/feifood/view/CadastroFrame.java
@@ -244,31 +244,6 @@ public class CadastroFrame extends javax.swing.JFrame {
         this.BackgroundPanel = BackgroundPanel;
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new CadastroFrame().setVisible(true));
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BackgroundPanel;
     private javax.swing.JButton CriarContaButton;

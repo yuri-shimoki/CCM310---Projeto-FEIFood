4ae0cf1f92a93554fed336c6a6393172d81ed23a
unifyshimoki <unifyshimoki@P2LB1007.FEINET.FEI.EDU.BR>
2025-11-13 14:39:15 -0300

    Adicionado javadoc e jar

--- FEIFood/src/feifood/dao/VerificarTabela.java
+++ FEIFood/src/feifood/dao/VerificarTabela.java
@@ -11,7 +11,7 @@ import java.sql.ResultSet;
 public class VerificarTabela
 {
     /**
-     * Verifica se uma tabela está vazia
+     * Verifica se uma tabela está vazia.
      * 
      * @param nomeTabela nome da tabela a ser checada
      * @return <code>true<code> se estiver vazia, senão <code>false</code>
